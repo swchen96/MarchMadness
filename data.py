ee65c4ac744dc2815646bd0c4b124410274a5fd0
Cameron Korb <cameronk@mit.edu>
2017-11-26 16:55:49 -0500

    stats for 15-16

--- data.py
+++ data.py
@@ -2,18 +2,22 @@ import pandas as pd
 import numpy as np
 from pprint import pprint as pprint
 
-data = pd.read_csv('team_stats.csv')
-#pprint(data)
+data = pd.read_csv('team_stats_16-17.csv')
+teams = data[['Team name', 'Team ID']]
+#pprint(teams)
 
-new_stats = pd.read_csv('rankings.csv').dropna()
+data = pd.read_csv('team_stats_15-16.csv')
+
+new_stats = pd.read_csv('rankings.csv').dropna()#.sort_values('Name').reset_index(drop = 'True')
+#new_stats['Games'] = new_stats['W'] + new_stats['L']
+
+
+
+new_stats = new_stats[['Name', 'PTS', 'PPG', 'OPP PTS', 'OPP PPG', 'SCR MAR']]
+new_stats.columns = ['Team name', 'PTS', 'PPG', 'OPP PTS', 'OPP PPG', 'SCR MAR']
 #pprint(new_stats)
 
+full_data = data.merge(new_stats, on='Team name', how='left')#, on=)
+pprint(full_data)
 
-new_stats = new_stats[['Name', '3FGA']]
-new_stats.columns = ['Team name', '3FGA']
-#pprint(new_stats)
-
-full_data = data.merge(new_stats, how='left')#, on=)
-#pprint(full_data)
-
-full_data.to_csv('team_stats.csv')
+full_data.to_csv('team_stats_15-16.csv')
