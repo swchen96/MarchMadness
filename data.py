6b3002042900cb99b4b7348a350c109ce40aa029
Cameron Korb <cameronk@mit.edu>
2017-11-26 20:56:16 -0500

    added 14-15 stats

--- data.py
+++ data.py
@@ -4,13 +4,12 @@ from pprint import pprint as pprint
 #
 # data = pd.read_csv('team_stats_16-17.csv')
 # teams = data[['Team name', 'Team ID']]
-# #pprint(teams)
+# pprint(teams)
 
 data = pd.read_csv('team_stats_14-15.csv')
 
 new_stats = pd.read_csv('rankings.csv').dropna()#.sort_values('Name').reset_index(drop = 'True')
 #new_stats['Games'] = new_stats['W'] + new_stats['L']
-
 
 
 new_stats = new_stats[['Name', 'Fouls', 'PFPG']]
